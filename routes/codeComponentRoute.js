820e096ed5c367f9456839c3a96ed48936cad77f
Akshay200228 <akshayrs096@gmail.com>
2023-12-10 13:25:46 +0530

    Fix issues

--- routes/codeComponentRoute.js
+++ routes/codeComponentRoute.js
@@ -17,23 +17,23 @@ const router = express.Router();
 // Create a new code component
 router.post('/', authenticate, createCodeComponent);
 
+// Get a single code component by ID
+router.get('/:codeComponentId', getSingleCodeComponent);
+
 // Get all code components
 router.get('/', getAllCodeComponents);
 
 // Get code components by category
 router.get('/:category', getCodeComponentsByCategory);
 
-// Get a single code component by ID
-router.get('/:codeComponentId', getSingleCodeComponent);
-
 // Fetch multiple code components by IDs (Note the change in the route parameter definition)
 router.get('/ids/:codeComponentIds*', getCodeComponentsByIds);
 
 // Update a code component by ID
-router.put('/:codeComponentId', authenticate, updateCodeComponent);
+router.put('/update/:codeComponentId', authenticate, updateCodeComponent);
 
 // Delete a code component by ID
-router.delete('/:codeComponentId', authenticate, deleteCodeComponent);
+router.delete('/delete/:codeComponentId', authenticate, deleteCodeComponent);
 
 
 export default router;
